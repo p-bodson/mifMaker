d36afcfbe2c000ccc79f1022b4d7d07e55af58a4
p-bodson <lkgombart@gmail.com>
2020-10-28 20:29:30 -0700

    changed file name

--- mifMaker.py
+++ mifMaker.py
@@ -1,11 +1,7 @@
 from matplotlib import image
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import numpy as np
 
-#img = input('Enter name of image in current directory: ')
-# load image as pixel array
-data = image.imread('../flamguin.jpg')
-# summarize shape of the pixel array
 def summarize(photo):
    print(f'data type = {data.dtype}')
    print(f'data shape = {data.shape}')
@@ -13,16 +9,11 @@ def summarize(photo):
    print(f'width (horizontal/column) of image = {data.shape[1]}')
    print(f'number of {data.dtype} per pixel of image = {data.shape[2]}')
 
-summarize(data)
-
 def float_to_u4(photo):
    return  (photo*15).round().astype(np.uint8)
 
 def u8_to_u4(photo):
    return (photo/255*15).round().astype(np.uint8)
-
-float_data = float_to_u4(data)
-u4_data = u8_to_u4(data)
 
 def print_row_4_float(photo, row):
    coord = [None,None,None,None] 
@@ -38,10 +29,25 @@ def print_row_3_u4(photo, row):
          coord[num] = photo[row][value][num]
       print(f'{row:x}-{value:x}: {coord[0]:x}{coord[1]:x}{coord[2]:x}')
 
+
+#img = input('Enter name of image in current directory: ')
+# load image as pixel array
+data = image.imread('images/knight_4.png')
+# summarize shape of the pixel array
+
+
+summarize(data)
+
+float_data = float_to_u4(data)
+u4_data = u8_to_u4(data)
+
+
 #print_row_4_float(float_data, 100)
-print_row_3_u4(u4_data, 100)
+#print_row_3_u4(u4_data, 100)
 
 
 # display the array of pixels as an image
-#pyplot.imshow(data)
-#pyplot.show()
+plt.imshow(data)
+plt.show()
+plt.imshow(float_data)
+plt.show()
