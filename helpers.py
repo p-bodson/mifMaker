2ac35ec0dbc8d9157879c2adc00aa4c77a4c2986
p-bodson <lkgombart@gmail.com>
2020-11-11 13:18:05 -0800

    added mif depth maximum to 65536 and image crop to 256x256 pixels

--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ def summarize(photo, title='DATA:'):
     #print(f'array: {photo}') 
     print(f'data type = {photo.dtype}')
     print(f'data shape = {photo.shape}')
-    print(f'data size = {photo.size}')
+    print(f'number of words = {photo.shape[0]*photo.shape[1]}')
     #print(f'data dimension = {photo.ndim}')
     print(f'height (vertical/row) of image = {photo.shape[0]}')
     print(f'width (horizontal/column) of image = {photo.shape[1]}')
@@ -17,17 +17,44 @@ def summarize(photo, title='DATA:'):
 # TODO make adding the alpha channel optional
 def resize(photo):
    # convert 3-channel rgb to 4-channel rgba
-   def rgb_to_rgba(photo, value=255):
-      alpha_channel = np.zeros((photo.shape[0],photo.shape[1],1), dtype=photo.dtype) + value
-      return np.concatenate((photo,alpha_channel), axis=2)
+   def rgb_to_rgba(data, value=255):
+      alpha_channel = np.zeros((data.shape[0],data.shape[1],1), dtype=data.dtype) + value
+      return np.concatenate((data,alpha_channel), axis=2)
 
-   if len(photo.shape) < 3:
-        print(f'''I don't know how to handle luminance data''')
-        return None
-   elif photo.shape[2] == 3:
-        return rgb_to_rgba(photo)
-   else:
-        return photo
+   # crop image dimensions to 256 pixels if any dimension is larger than 256
+   def crop(data):      
+      # check vertical
+      if data.shape[0] > 256:
+         print(f'ATTENTION: The image height is too large and will be cropped to 256 pixels')
+         cropped_data = data[0:256,:,:,]
+      
+      # check horizontal
+      elif data.shape[1] > 256:
+         print(f'ATTENTION: The image width is too large and will be cropped to 256 pixels')
+         cropped_data = data[:,0:256,:,]
+      
+      # else do nothing
+      else:
+         cropped_data = data
+      
+      # rerun crop for next dimension
+      if cropped_data.shape[1] > 256:
+         return crop(cropped_data)
+      else:
+         return cropped_data
+
+   # making sure the photo has 4 channels for pixel
+   def check_dim(data):
+      if len(data.shape) < 3:
+         print(f'''I don't know how to handle luminance data''')
+         return None
+      elif data.shape[2] == 3:
+         return rgb_to_rgba(data)
+      else:
+         return data
+
+   image = crop(photo)
+   return check_dim(image)
 
 #TODO how to handle exceptions?
 def prepare_data(photo):
@@ -110,6 +137,7 @@ def write_mif(photo, mode='x', file='mifData.mif'):
    '''
    this writes out the mif
    '''
+
    f = open(file, mode)
    f.write(form_mif(photo))
    f.close()
--- mifMaker.py
+++ mifMaker.py
@@ -6,16 +6,11 @@ from helpers import summarize, create_u4, create_preview, write_mif
 img = input('Enter name of image relative to current directory: ')
 data = image.imread(img)
 
-# make array into appropriate size for next steps
-#new_data = prepare_data(data)
-
 # create data for VGA from image array
 uint4_data = create_u4(data)
 
 summarize(data, 'ORIGINAL DATA:')
 summarize(uint4_data, 'MIF DATA')
-#summarize(new_data, 'PREPARED DATA:')
-#summarize(data, 'ORIGINAL DATA:')
 
 # save converted image for preview before placing on FPGA
 create_preview(uint4_data)
